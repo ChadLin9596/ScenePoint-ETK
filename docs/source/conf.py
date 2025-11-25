889b409a0b10af2e79400493d5746756cbc0849d
Chad Lin <chun-jung.lin@adelaide.edu.au>
2025-11-25 17:55:01 +1030

    updated website for citation and added installation and download for documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,27 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'scene_point_etk'
-copyright = '2025, Chad Lin'
-author = 'Chad Lin'
-release = 'v.0.1.0'
+project = "scene_point_etk"
+copyright = "2025, Chad Lin"
+author = "Chad Lin"
+release = "v.0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "haiku"
+html_static_path = ["_static"]
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -6,12 +6,24 @@
 scene_point_etk documentation
 =============================
 
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
+.. note::
+   This project is under active development. Documentation may change frequently.
+
+`GitHub Repository`_
+
+Getting Started
+===============
+
+.. toctree::
+   :maxdepth: 1
+
+   installation
+   download
 
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
+.. comment: list link used in the file above
+.. _GitHub Repository: https://github.com/ChadLin9596/ScenePoint-ETK
